a5a3ea83c711406492abb1d185b179159d612d19
vmlaine <bhe446@myy.haaga-helia.fi>
2023-09-27 11:48:32 +0300

    rent per participant added

--- 05.09SummerCottageEx.js
+++ 05.09SummerCottageEx.js
@@ -1,5 +1,13 @@
 function calculateRent() {
 		
 		 // TODO
+		 let rent = document.getElementById("rent").value;
+		 let participants = document.getElementById("participants").value;
+
+         let rentPerParticipant = rent / participants;
+		 let roundedRentPerPart = rentPerParticipant.toFixed(2);
+
+		 let answerDiv = document.getElementById("answerDiv");
+		answerDiv.textContent = `Rent per participants is ${roundedRentPerPart} euros.`;
 		
 }
