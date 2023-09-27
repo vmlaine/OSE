1a82ad2f86b85102cc379a4768f4511f4a591e4d
vmlaine <bhe446@myy.haaga-helia.fi>
2023-09-27 12:13:25 +0300

    Car share added

--- 05.10CarShareEx.js
+++ 05.10CarShareEx.js
@@ -1,4 +1,14 @@
 function calculateCosts() {
 
 		//TODO
+		let drivenKilometers = document.getElementById("kilometers").value;
+		let consumption = document.getElementById("consumption").value;
+		let fuelPrice = document.getElementById("price").value;
+		let participants = document.getElementById("participants").value;
+
+		let costPerParticipant = consumption / 100 * fuelPrice * drivenKilometers / participants;
+		let roundedCostPerPart = costPerParticipant.toFixed(2);
+
+		let answerId = document.getElementById("answerId");
+		answerId.textContent = `Fuel costs per participant is ${roundedCostPerPart} euros.`;
 }
