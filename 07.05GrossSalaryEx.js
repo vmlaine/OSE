6b7b77ce2d183efc4a8e942a7cf880cfd5053bb6
vmlaine <bhe446@myy.haaga-helia.fi>
2023-10-11 11:32:31 +0300

    07 section done

--- 07.05GrossSalaryEx.js
+++ 07.05GrossSalaryEx.js
@@ -1,15 +1,19 @@
 function calculateSalary(hours, hourlySalary) {
 		// calculate salary
-
+        let grossSalary = hours * hourlySalary;
 		// return the gross salary with the return statement
-		
+		return grossSalary;
 }
 
 function showGrossSalary() {
 		// assign the input field value to the variable hours
-		// assign the second input field value to the variable hourlySalary
-		// call the calculateSalary function
-		// write the answer on the html page
+		let hours = document.getElementById("hours").value;
 
+		// assign the second input field value to the variable hourlySalary
+		let hourlySalary = document.getElementById("hourlySalary").value;
+		// call the calculateSalary function
+		let grossSalary = calculateSalary (hours, hourlySalary);
+		// write the answer on the html page
+		document.getElementById("answer").innerHTML = `Gross salary is ${grossSalary.toFixed(2)} euros.`;
 }
 
