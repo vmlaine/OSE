1f64475e70ce24fedc39228cba5547efd3b31d87
vmlaine <bhe446@myy.haaga-helia.fi>
2023-10-12 12:56:08 +0300

    String assignment done

--- 07.07StringEx.js
+++ 07.07StringEx.js
@@ -1,22 +1,29 @@
 function processName() {
 
     // read value of name from the input field
-   
+    let name = document.getElementById("name").value;
 
     // show it with "ALL CAPS" using toUpperCase()
-   
+    document.getElementById("upperCase").innerHTML = `With upper case letters: ${name.toUpperCase()}` ;
 
     // show it with  "all small letters" using toLowerCase()
- 
+    document.getElementById("lowerCase").innerHTML = `With lower case letters: ${name.toLowerCase()}`;
 
-    //tell how many characters are there (length, includes also all spaces) 
-   
+    //tell how many characters are there (length, includes also all spaces)
+    document.getElementById("length").innerHTML = `Character count: ${name.length}`;
 
-    // tell, whether the input string contains the word 'muumi'
-    
+    //tell, whether the input string contains the word 'muumi'
+    let containsMuumi = name.includes("muumi");
+    if (containsMuumi == true) {
+        document.getElementById("contains").innerHTML = `Contains the text muumi.`;
+    } else {
+        document.getElementById("contains").innerHTML = `Doesn't contain the text muumi.`;
+    }
+
     // tell the first character using charAt()
-    
+    document.getElementById("firstCharacter").innerHTML = `First character: ${name.charAt(0)}`;
 
     // tell which ones are the first three characters using substring().
-   
+    let firstThreeCharacters = name.substring(0, 3);
+    document.getElementById("manyCharacters").innerHTML = `First three characters: ${firstThreeCharacters}`;
 }
---
+++ 07.09AgeEx.js
@@ -0,0 +1,20 @@
+function tellAge() {
+    // Read value of yearOfBirth from the input field 
+   
+
+    // Figure out the current date into today variable 
+  
+
+    // Use getFullYear() method to get the current year out from the today variable
+   
+
+    // Calculate the (rough estimate of the) age
+    
+
+    // If age is less than zero
+ 
+        //  Show an error message
+        
+    // otherwise tell the age
+   
+}
