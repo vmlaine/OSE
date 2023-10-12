ba757be0c97aa0848423339ac288baf2006bbfb3
vmlaine <bhe446@myy.haaga-helia.fi>
2023-10-12 13:38:18 +0300

    Age assignment done

--- 07.09AgeEx.js
+++ 07.09AgeEx.js
@@ -1,20 +1,23 @@
 function tellAge() {
     // Read value of yearOfBirth from the input field 
-   
+    let yearOfBirth = document.getElementById("yearOfBirth").value;
 
     // Figure out the current date into today variable 
-  
+    let today = new Date();
 
     // Use getFullYear() method to get the current year out from the today variable
-   
+    let currentYear = (today.getFullYear());
 
     // Calculate the (rough estimate of the) age
-    
+    let age = currentYear - yearOfBirth;
 
     // If age is less than zero
- 
+    if (age < 0) {
         //  Show an error message
+        document.getElementById("answer").innerHTML = `Your age can't be negative!`;
         
     // otherwise tell the age
-   
+    }   else {
+            document.getElementById("answer").innerHTML = `You are now ` + age + ` years old.<br> (Current year is ` + currentYear + `.)`;
+    }
 }
---
+++ 07.10PriceOfWorkEx.js
@@ -0,0 +1,52 @@
+function modifyTitle() {
+
+    // Read value of title from the input field
+    // Call allCapsTitleTrimmed() function 
+
+
+}
+
+function allCapsTitleTrimmed(text) {
+	// Removes any empty white space from the beginning or the end of the text
+    
+	// Changes the text given to it to be ALL CAPS
+    
+	// Return the changed text
+	
+}
+
+function calculate() {
+
+    // Calculate length of the work as minutes.
+
+    // If it is not Sunday  (Call isSunday() function)
+        //Calculate and show price of the repair work during the workdays
+    //otherwise			
+        //Calculate and showprice of the repair work on Sundays
+
+}
+
+ // The function gets the date text in the format "dd.MM.yyyy". 
+ // The function returns Boolean value true if the date is for a Sunday, otherwise false.
+function isSunday(dateText) {
+	
+	// Split the given date text into day, month and year parts using the subStr() method
+	
+
+	// Create a new Date object to a variable asDate
+	
+
+	// Get the day of the week with its getDay() method. Sunday is number 0 .
+	
+
+	// If day of the week is Sunday 
+	
+		// return Boolean value true
+		
+	// otherwise	
+	
+       // return Boolean value false		
+	
+}
+
+
